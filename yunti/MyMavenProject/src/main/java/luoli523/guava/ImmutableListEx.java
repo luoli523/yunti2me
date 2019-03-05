b18e7912ad4ceffaba6ac1c8850695b0f3c972be
luoli <luoli523@gmail.com>
2019-03-05 10:42:57 +0800

    finish the immutable list example code

--- yunti/MyMavenProject/src/main/java/luoli523/guava/ImmutableListEx.java
+++ yunti/MyMavenProject/src/main/java/luoli523/guava/ImmutableListEx.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableList;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -12,11 +13,13 @@ public class ImmutableListEx {
   public static void main(String[] args) {
     unmodifiableCollectionExample();
     immutableListOfExample();
+    immutableListCopyOfExample();
+    immutableListBuilderExample();
   }
 
   /**
-     Collections.unmodifiableList() 返回的只是这个List的一个不可变视图，
-     如果原List中有元素被修改，其视图中的元素也会同样被影响到
+   * Collections.unmodifiableList() 返回的只是这个List的一个不可变视图，
+   * 如果原List中有元素被修改，其视图中的元素也会同样被影响到
    */
   private static void unmodifiableCollectionExample() {
     System.out.println("=====================================");
@@ -53,14 +56,52 @@ public class ImmutableListEx {
     System.out.println("=====================================");
   }
 
+  private static void immutableListCopyOfExample() {
+    System.out.println("=====================================");
+
+    Integer[] array = new Integer[] {1,2,3};
+    List<Integer> immutableListCopiedFromArray = ImmutableList.copyOf(array);
+    verifyElements("immutableListCopiedFromArray", immutableListCopiedFromArray);
+
+    List<Integer> list = Arrays.asList(array);
+    List<Integer> immutableListCopiedFromList = ImmutableList.copyOf(list);
+    verifyElements("immutableListCopiedFromList", immutableListCopiedFromList);
+
+    final Iterator<Integer> it = list.iterator();
+    List<Integer> immutableListCopiedFromIterator = ImmutableList.copyOf(it);
+    verifyElements("immutableListCopiedFromIterator", immutableListCopiedFromIterator);
+
+    Iterator<Integer> iterable = new MyIterable<Integer>(it);
+    List<Integer> immutableListCopieFromIterable = ImmutableList.copyOf(iterable);
+    // 这里由于it在构建immutableListCopieFromIterable的时候已经遍历过一次，所以构建出来的immutableListCopieFromIterable是一个空队列
+    verifyElements("immutableListCopiedFromAlreadyTraversedIterable", immutableListCopieFromIterable);
+    // 这里使用一个新的迭代器，就可以重新构建一个包含所有元素的队列
+    verifyElements("immutableListCopiedFromFreshIterable", ImmutableList.copyOf(list.iterator()));
+
+    System.out.println("=====================================");
+  }
+
+  private static void immutableListBuilderExample() {
+    List<Integer> list = Arrays.asList(new Integer[] {1, 2, 3});
+    ImmutableList.Builder<Integer> builder = ImmutableList.builder();
+    ImmutableList<Integer> immutableListFromBuilder = builder
+            .add(1)
+            .add(2, 3)
+            .addAll(list)
+            .addAll(list.iterator())
+            .addAll(new MyIterable<Integer>(list.iterator()))
+            .build();
+    verifyElements("immutableListFromBuilder", immutableListFromBuilder);
+  }
+
   private static <T> void verifyElements(String name, List<T> list) {
     System.out.print("List Name: " + name);
-    if(list.isEmpty()) {
+    if (list.isEmpty()) {
       System.out.println(" is empty");
     } else {
       System.out.print((list.size() == 1 ? " has one element: " : " has "
               + list.size() + " elements: "));
-      if(list.size() == 1) {
+      if (list.size() == 1) {
         System.out.println(list.get(0));
       } else {
         list.forEach(new Consumer<T>() {
@@ -70,6 +111,30 @@ public class ImmutableListEx {
         });
         System.out.println();
       }
+    }
+  }
+
+  private static class MyIterable<E> implements Iterator<E> {
+    private Iterator<E> itr;
+
+    public boolean hasNext() {
+      return itr.hasNext();
+    }
+
+    public void remove() {
+      itr.remove();
+    }
+
+    public E next() {
+      return itr.next();
+    }
+
+    MyIterable(Iterator<E> itr) {
+      this.itr = itr;
+    }
+
+    public Iterator<E> iterator() {
+      return itr;
     }
   }
 
