ab4633d420bd5ca99e0d9455217b237e6d982505
luoli <luoli523@gmail.com>
2019-03-08 10:56:04 +0800

    lambda test

--- yunti/MyMavenProject/src/main/java/luoli523/java8/lambda/LambdaTest.java
+++ yunti/MyMavenProject/src/main/java/luoli523/java8/lambda/LambdaTest.java
@@ -21,7 +21,7 @@ class Something {
 // We can use arbitrary interfaces as lambda expressions as long as the interface only contains one abstract method.
 // To ensure that your interface meet the requirements, you should add the @FunctionalInterface annotation.
 // The compiler is aware of this annotation and throws a compiler error as soon as you try to
-// add a second abstract method declaration to the interface.
+// add a second abstract method declaration to the interfaceR.
 @FunctionalInterface
 interface Converter<F, T> {
   T convert(F from);
