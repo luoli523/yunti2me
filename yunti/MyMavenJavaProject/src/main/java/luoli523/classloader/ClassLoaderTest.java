fc96bfac5221692365fdc4f6e3b16a05d2420668
luoli <luoli523@gmail.com>
2019-03-15 10:37:18 +0800

    add SynchronizedTest

--- yunti/MyMavenJavaProject/src/main/java/luoli523/classloader/ClassLoaderTest.java
+++ yunti/MyMavenJavaProject/src/main/java/luoli523/classloader/ClassLoaderTest.java
@@ -2,6 +2,27 @@ package luoli523.classloader;
 
 public class ClassLoaderTest {
 
+  /*
+   The application class loader loads the class where the example method is contained.
+   An application or system class loader loads our own files in the classpath.
+   eg: JAVA_OME/rt.jar
+
+   Next, the extension one loads the Logging class.
+   Extension class loaders load classes that are an extension of the standard core Java classes.
+   eg: JAVA_OME/lib/ext/*.jar
+
+   Finally, the bootstrap one loads the ArrayList class.
+   A bootstrap or primordial class loader is the parent of all the others.
+   eg: your/path/to/app.jar in the -classpath
+
+   However, we can see that the last out, for the ArrayList it displays null in the output.
+   This is because the bootstrap class loader is written in native code,
+   not Java – so it doesn’t show up as a Java class. Due to this reason,
+   the behavior of the bootstrap class loader will differ across JVMs.
+   */
+  // 1, bootstrap classloader
+  // 2, extension classloader
+  // 3, application classloader
   public static void main(String[] args) {
     System.out.println("class loader for HashMap: " + java.util.HashMap.class.getClassLoader());
     System.out.println("class loader for DNSNameService: " + sun.net.spi.nameservice.dns.DNSNameService.class.getClassLoader());
---
+++ yunti/MyMavenJavaProject/src/test/java/luoli523/backtobasic/SynchronizedTest.java
@@ -0,0 +1,53 @@
+package luoli523.backtobasic;
+
+import org.junit.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+class LuoliSynchronizedMethods {
+  private int sum = 0;
+
+  public int getSum() {
+    return sum;
+  }
+
+  public void setSum(int sum) {
+    this.sum = sum;
+  }
+
+  public void calculate() {
+    setSum(getSum()+1);
+  }
+
+  public synchronized void synchronizedCalculate() {
+    setSum(getSum()+1);
+  }
+}
+
+public class SynchronizedTest {
+
+  @Test
+  public void givenMultiThread_whenNonSyncMethod() throws InterruptedException {
+    LuoliSynchronizedMethods cal = new LuoliSynchronizedMethods();
+    ExecutorService service = Executors.newFixedThreadPool(3);
+    IntStream.range(0,1000).forEach( i -> service.submit(cal::calculate));
+    service.awaitTermination(1000, TimeUnit.MILLISECONDS);
+    assertNotEquals(1000, cal.getSum());
+  }
+
+  @Test
+  public void givenMultiThread_whenSyncMethod() throws InterruptedException {
+    LuoliSynchronizedMethods cal = new LuoliSynchronizedMethods();
+    ExecutorService service = Executors.newFixedThreadPool(3);
+    IntStream.range(0,1000).forEach( i -> service.submit(cal::synchronizedCalculate));
+    service.awaitTermination(1000, TimeUnit.MILLISECONDS);
+    assertEquals(1000, cal.getSum());
+  }
+
+}
