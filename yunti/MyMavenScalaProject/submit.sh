654528672d4c0146f0847de5c9c84974f4d9b9b3
luoli <luoli523@gmail.com>
2019-03-21 12:02:02 +0800

    project name changes

--- yunti/MyMavenScalaProject/submit.sh
+++ yunti/MyMavenScalaProject/submit.sh
@@ -4,7 +4,7 @@ home="${HOME}"
 spark_home="${HOME}/soft/spark-2.4.0-bin-hadoop2.7"
 submit_script="$spark_home/bin/spark-submit"
 submit_dir="${HOME}/dev/git/luoli/yunti2me/yunti/MyMavenScalaProject"
-submit_jar="scala-maven-example-1.0.0-SNAPSHOT.jar"
+submit_jar="MyMavenScalaProject-1.0.jar"
 
 clz=$1
 shift
